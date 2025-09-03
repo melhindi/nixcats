ccf308e080cd454545f9ac7b2f13c57d738a1836
melhindi <mu7eh7@gmail.com>
2025-09-03 11:56:31 +0200

    change package name to nixcats with nvim as alias

--- flake.nix
+++ flake.nix
@@ -225,7 +225,7 @@
     packageDefinitions = {
       # These are the names of your packages
       # you can include as many as you wish.
-      nvim = {pkgs, ...}: {
+      nixcats = {pkgs, ...}: {
         # they contain a settings set defined above
         # see :help nixCats.flake.outputs.settings
         settings = {
@@ -234,7 +234,7 @@
           wrapRc = true;
           # IMPORTANT:
           # your alias may not conflict with your other packages.
-          aliases = ["nixcats"];
+          aliases = ["nvim"];
           # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
         };
         # and a set of categories that you want
@@ -251,7 +251,7 @@
     };
     # In this section, the main thing you will need to do is change the default package name
     # to the name of the packageDefinitions entry you wish to use as the default.
-    defaultPackageName = "nvim";
+    defaultPackageName = "nixcats";
   in
     # see :help nixCats.flake.outputs.exports
     forEachSystem (system: let
