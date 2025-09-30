e746794cd66b4d83d27d716faf495c5e46f21e46
melhindi <mu7eh7@gmail.com>
2025-09-30 10:37:15 +0200

    Migrate lspconfig to new framework

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -20,52 +20,54 @@ end
 servers.nixd = {}
 servers.lua_ls = {}
 
-local lspconfig = require('lspconfig')
-lspconfig.util.default_config = vim.tbl_extend(
-  "force",
-  lspconfig.util.default_config,
-  {
-    capabilities = require('blink.cmp').get_lsp_capabilities({}, true),
-    on_attach = function(_, bufnr)
-      -- we create a function that lets us more easily define mappings specific
-      -- for LSP related items. It sets the mode, buffer and description for us each time.
-      local nmap = function(keys, func, desc)
-        if desc then
-          desc = 'LSP: ' .. desc
-        end
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-      end
-      local telescope = require("telescope.builtin");
-      nmap('<leader>cn', vim.lsp.buf.rename, '[R]e[n]ame')
-      nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-      nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-      nmap('<leader>d', vim.lsp.buf.type_definition, 'Type [D]efinition')
-      nmap('<leader>r', telescope.lsp_references, '[G]oto [R]eferences')
-      nmap('<leader>i', telescope.lsp_implementations, '[G]oto [I]mplementation')
-      nmap('<leader>s', telescope.symbols, '[D]ocument [S]ymbols')
-      nmap('<leader>ws',  telescope.lsp_workspace_symbols , '[W]orkspace [S]ymbols')
+vim.lsp.config('*', {
+  capabilities = require('blink.cmp').get_lsp_capabilities({}, true),
+  on_attach = function(_, bufnr)
 
-      -- See `:help K` for why this keymap
-      nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-      nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+    -- create a function to more easily define mappings specific
+    -- for LSP related items. It sets the mode, buffer and description for us each time.
+    local nmap = function(keys, func, desc)
+      if desc then desc = 'LSP: ' .. desc end
+      vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
 
-      -- Lesser used LSP functionality
-      nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-      nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-      nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-      nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, '[W]orkspace [L]ist Folders')
+    local telescope = require('telescope.builtin')
 
-      -- Create a command `:Format` local to the LSP buffer
-      vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-      end, { desc = 'Format current buffer with LSP' })
-    end,
-  }
-)
--- set up the servers to be loaded on the appropriate filetypes!
-for server_name, cfg in pairs(servers) do
-  lspconfig[server_name].setup(cfg or {})
+    nmap('<leader>cn', vim.lsp.buf.rename, '[R]e[n]ame')
+    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    nmap('<leader>d', vim.lsp.buf.type_definition, 'Type [D]efinition')
+    nmap('<leader>r', telescope.lsp_references, '[G]oto [R]eferences')
+    nmap('<leader>i', telescope.lsp_implementations, '[G]oto [I]mplementation')
+
+    nmap('<leader>s', telescope.lsp_document_symbols, '[D]ocument [S]ymbols')
+    nmap('<leader>ws', telescope.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+    -- See `:help K` for why this keymap
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    nmap('<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[W]orkspace [L]ist Folders')
+
+    -- Create a command `:Format` local to the LSP buffer
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+      vim.lsp.buf.format()
+    end, { desc = 'Format current buffer with LSP' })
+  end,
+})
+
+-- Server-specific configs + enable by filetype
+-- `servers` should be a table like: { lua_ls = {settings=...}, pyright = {}, ... }
+for name, cfg in pairs(servers) do
+  if cfg and next(cfg) ~= nil then
+    -- Merge/override defaults for this server
+    vim.lsp.config(name, cfg)
+  end
+  -- Activate (auto-starts on matching filetypes/root)
+  vim.lsp.enable(name)
 end
-
