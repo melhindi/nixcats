724b609e430eafbd5aa9d31d7dece8a8309e45a9
melhindi <mu7eh7@gmail.com>
2026-06-06 13:27:38 +0200

    Improve flash config

--- plugin/flash.lua
+++ plugin/flash.lua
@@ -5,6 +5,10 @@ end
 local flash = require("flash")
 
 flash.setup({
+  remote_op = {
+    restore = true,
+    motion = nil,
+  },
   modes = {
     treesitter = {
       labels = "asdfghjklqwertyuiopzxcvbnm",
@@ -12,6 +16,42 @@ flash.setup({
         before = true,
         after = true,
         style = "inline",
+        rainbow = {
+          enabled = true,
+          shade = 5,
+        },
+      },
+      highlight = {
+        backdrop = false,
+        matches = false,
+      },
+    },
+    remote = {
+      remote_op = {
+        restore = true,
+        motion = true,
+      },
+    },
+    treesitter_search = {
+      jump = {
+        pos = "range",
+      },
+      search = {
+        multi_window = true,
+        wrap = true,
+        incremental = false,
+      },
+      remote_op = {
+        restore = true,
+      },
+      label = {
+        before = true,
+        after = true,
+        style = "inline",
+        rainbow = {
+          enabled = true,
+          shade = 5,
+        },
       },
       highlight = {
         backdrop = false,
@@ -26,13 +66,22 @@ vim.keymap.set({ "n", "x", "o" }, "s", function()
 end, { desc = "Flash" })
 
 vim.keymap.set({ "n", "x", "o" }, "S", function()
-  flash.treesitter()
-end, { desc = "Flash treesitter" })
+  flash.treesitter({
+    actions = {
+      ["<C-Space>"] = "next",
+      ["<BS>"] = "prev",
+    },
+  })
+end, { desc = "Flash Treesitter" })
 
 vim.keymap.set("o", "r", function()
   flash.remote()
-end, { desc = "Remote flash" })
+end, { desc = "Remote Flash" })
 
 vim.keymap.set({ "o", "x" }, "R", function()
   flash.treesitter_search()
-end, { desc = "Treesitter search" })
+end, { desc = "Treesitter Search" })
+
+vim.keymap.set("c", "<C-s>", function()
+  flash.toggle()
+end, { desc = "Toggle Flash Search" })
